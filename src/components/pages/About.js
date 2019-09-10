b69c125b8c5ea6cf50f96e3730e94a3f5ed4977d
Binh Nguyen <b.nguyen.sn@gmail.com>
2019-09-11 00:19:46 +0100

    Update the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import './about.css';
 
 export default function About() {
-  return <div className="about">About</div>;
+  return (
+    <div className="about">
+      <div className="about-content-container">
+        <div>
+          This is a <a href="https://reactjs.org/">React</a> application. It
+          uses data from Blizzard's{' '}
+          <a href="https://overwatchleague.com/">Overwatch League</a> API.
+        </div>
+        <div>
+          Source code is public on{' '}
+          <a href="https://github.com/bnguyensn/ow-champion-belt-tracker">
+            GitHub
+          </a>
+          .
+        </div>
+        <div>
+          Made with 💖 by <a href="https://twitter.com/bnguyensn">@bnguyensn</a>
+          .
+        </div>
+      </div>
+    </div>
+  );
 }
