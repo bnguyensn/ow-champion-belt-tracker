8f0fa8570b84511a2aa177481c312604deb762f0
Binh Nguyen <b.nguyen.sn@gmail.com>
2019-09-11 00:18:27 +0100

    Center <Spinner> component

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,5 +2,9 @@ import React from 'react';
 import './spinner.css';
 
 export default function Spinner() {
-  return <div className="spinner" />;
+  return (
+    <div className="spinner-container">
+      <div className="spinner" />
+    </div>
+  );
 }
